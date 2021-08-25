24173ce4b7e34d3405c73a0e3a726bacb8c73121
Muhammad Asif <m.asif@qbatch.com>
2021-08-25 16:03:35 +0500

    Change the default state value with 100 with addition to the previous value

--- react-redux/redux-app/src/reducers/upDown.js
+++ react-redux/redux-app/src/reducers/upDown.js
@@ -3,7 +3,7 @@ const changeTheNumber = (state = initialState, action) => {
     switch (action.type) {
         case "INCREMENT": return state + 1;
         case "DECREMENT": return state - 1;
-        default: return state;
+        default: return state + 100;
     }
 }
 export default changeTheNumber;
